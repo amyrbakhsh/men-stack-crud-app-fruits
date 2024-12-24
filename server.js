0f9705e487f01bd67de5cb13799607ab2bec98dc
amyrbakhsh <amyr.bakhsh@gmail.com>
2024-12-24 18:53:25 +0300

    add create index page

--- server.js
+++ server.js
@@ -15,18 +15,39 @@ const Fruit = require("./models/fruit.js");
 
 // MIDDLEWARE
 app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+
 
 // ROUTES
 
 
 app.get("/", async (req, res) => {
     res.render("index.ejs");
-  });
+    }
+);
 
-  app.get("/fruits/new", (req, res) => {
+    // Get fruits/new
+app.get("/fruits/new", (req, res) => {
     res.render("fruits/new.ejs");
-  });
-  
+    }
+);
+
+//Get /fruits
+app.get('/fruits', async (req,res) => {
+    const allFruits = await Fruit.find();
+    res.render( 'fruits/index.ejs' ,{ fruits: allFruits })
+})
+
+app.post("/fruits", async (req, res) => {
+    if (req.body.isReadyToEat === "on") {
+      req.body.isReadyToEat = true;
+    } else {
+      req.body.isReadyToEat = false;
+    }
+    await Fruit.create(req.body);
+    res.redirect('/fruits');
+  }
+);
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
